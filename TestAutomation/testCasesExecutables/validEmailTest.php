980cf255b3a81c3442c3a71b0bdb2abc6872de30
Josh Gilley <joshdgilley@gmail.com>
2020-10-04 17:51:16 -0400

    (UPDATE) Added Test Cases and started building

--- TestAutomation/testCasesExecutables/validEmailTest.php
+++ TestAutomation/testCasesExecutables/validEmailTest.php
@@ -1,8 +1,35 @@
 <?php
-error_reporting(0);
+//error_reporting(0);
+define('CLI_SCRIPT', true);
+defined('MOODLE_INTERNAL') || die();
+require_once("../project/moodle1/config.php");
+require(__DIR__ . "../project/moodle1/search/classes/document.php");
+
+$testClass = new document( 23, 'twenty-three', 'twenty-three' );
+
+$final = $testClass->get("string");
+echo $final
+/*
+$newTest = new validEmailTest();
+$newTest->testUsername($argv[1]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class validEmailTest {
-
 
 function testUsername($name){
 $valid = False;
@@ -21,8 +48,10 @@ else {
 }
 
 }
-$newTest = new validEmailTest();
-$newTest->testUsername($argv[1]);
+*/
 
 ?>
+
+
+
 
