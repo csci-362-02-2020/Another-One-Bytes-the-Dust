09596a54e083148c4c07d46834562b919948371f
Alex <rosiea184@gmail.com>
2020-10-29 15:31:52 -0400

    Script updated again

--- TestAutomation/scripts/driver.py
+++ TestAutomation/scripts/driver.py
@@ -16,11 +16,11 @@ y = x.split('***')
 #print(y)
 for case in y:
 	attr = case.split('$$$')
-	yes = attr
-	print(attr)
+	yes = attr #trying to get what is printed to output to html file. 
+	print(attr) #how do we put what is being printed in a variable?
 
 
-text = f'''<!DOCTYPE html> <title>Test Report</title> <body> <h1>Test Report</h1> <br> {yes} </body> </html>'''
+text = f'''<!DOCTYPE html> <title>Test Report</title> <body> <h1>Test Report</h1> <br> {case} </body> </html>'''
 with open("testReport.html","w") as html_file:
 	html_file.write(text)
 
