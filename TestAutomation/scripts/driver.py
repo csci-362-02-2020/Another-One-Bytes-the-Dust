0546914bc7b601b462711b6d5a122e6b5707bcac
Alex <rosiea184@gmail.com>
2020-11-01 16:00:13 -0500

    Re did html output

--- TestAutomation/scripts/driver.py
+++ TestAutomation/scripts/driver.py
@@ -5,24 +5,51 @@ import subprocess
 import webbrowser
 import time
 
-
 result = subprocess.run(
 	['php', '../testCasesExecutables/TC01_05Driver.php'],
 	stdout=subprocess.PIPE,
 	check=True
 )
+
+
 x = str(result.stdout)
 y = x.split('***')
 #print(y)
+outstring = ""
 for case in y:
 	attr = case.split('$$$')
-	yes = attr #trying to get what is printed to output to html file. 
-	print(attr) #how do we put what is being printed in a variable?
+	for thing in attr
+		outstring += str(thing)
+	print(attr)
 
+text = wrapper % (outstring)
 
-text = f'''<!DOCTYPE html> <title>Test Report</title> <body> <h1>Test Report</h1> <br> {case} </body> </html>'''
 with open("testReport.html","w") as html_file:
 	html_file.write(text)
+
+wrapper = '''<!DOCTYPE html> 
+<head>
+<style>
+.container{
+	width: 90%;
+	margin-left: auto;
+	margin-right: auto;
+	height: 1000px;
+	background-color: #f2f2f2;
+	
+</style>
+<meta charset="utf-8">
+<title>Test Report</title> </head>
+<body> <div class="container"><h1>Test Report</h1><br>
+%s
+	</div>
+	</body> 
+	</html>'''
+	
+
+html_file.close()
+open_new_tab(testReport.html)
+
 
 
 
